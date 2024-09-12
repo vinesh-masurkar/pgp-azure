be04a15d4245893308bd1654e3ff4252ea048f99
Masurkar <vineshmasurkar@yahoo.com>
2024-09-11 23:25:38 -0400

    Update index.php
    Showing first 10 rows of the employees table

--- index.php
+++ index.php
@@ -1,10 +1,11 @@
-p<!DOCTYPE html>
+<!DOCTYPE html>
 <html>
 <head>
 	<title>MySQL Table Viewer</title>
 </head>
 <body>
 	<h1>MySQL Table Viewer</h1>
+	<h2> selecting Top 10 rows of the employees table</h2>
 	<?php
 		// Define database connection variables
 		$servername = "strangfigdbserver.mysql.database.azure.com";
@@ -26,10 +27,11 @@ p<!DOCTYPE html>
 
 		if ($result->num_rows > 0) {
 			// Display table headers
-			echo "<table><tr><th>ID</th><th>Name</th><th>Email</th></tr>";
+			echo "<table><tr><th>Emp-ID</th><th>First Name</th><th>Last Name</th><th>Email</th></tr>";
 			// Loop through results and display each row in the table
 			while($row = $result->fetch_assoc()) {
-				echo "<tr><td>" . $row["id"] . "</td><td>" . $row["name"] . "</td><td>" . $row["email"] . "</td></tr>";
+				echo "<tr><td>" . $row["emp_no"] . "</td><td>" . $row["first_name"] . "</td><td>" . $row["last_name"] . "</td><td>" . $row["email_id"] . "</td></tr>";
+
 			}
 			echo "</table>";
 		} else {
